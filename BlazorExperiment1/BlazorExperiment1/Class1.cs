c7de33f70500dfc4a955a4a27d5365adf4194d36
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-09-30 16:33:29 -0500

    added and modified a few random files

---
+++ BlazorExperiment1/BlazorExperiment1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorExperiment1
+{
+    public class Class1
+    {
+        public Class1(String passedString)
+        {
+            boundString = passedString;
+        }
+        public string boundString { get; set; }
+    }
+}
